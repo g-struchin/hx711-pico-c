e51e1433ffd3ddf7f1ea90ad3b4f00c48d8f2c6e
George <g.struchin@gmail.com>
2024-09-06 16:30:00 +0400

    old connection

--- tests/main.c
+++ tests/main.c
@@ -25,12 +25,25 @@
 #include "pico/stdio.h"
 #include "tusb.h"
 #include "../include/common.h"
+//                 4294967296
+//#define DELAY_US    480000000
+#define DELAY_US    20000000
 
 #define PRINT_ARR(arr, len) \
     do { \
         for(size_t i = 0; i < len; ++i) { \
             printf("hx711_multi_t chip %i: %li\n", i, arr[i]); \
         } \
+    } while(0)
+
+#define PRINT_ARR_SP(arr, len) \
+    do { \
+        printf("$%li %li %li %li;\n", arr[0], arr[1], arr[2], arr[3]); \
+    } while(0)
+
+#define PRINT_ARR_MathLab(arr, len) \
+    do { \
+        printf("[%li %li %li %li]\n", arr[0], arr[1], arr[2], arr[3]); \
     } while(0)
 
 int main(void) {
@@ -46,7 +59,7 @@ int main(void) {
     hx711_get_default_config(&hxcfg);
 
     hxcfg.clock_pin = 14;
-    hxcfg.data_pin = 15;
+    hxcfg.data_pin = 2;
 
     hx711_t hx;
 
@@ -54,7 +67,7 @@ int main(void) {
     hx711_init(&hx, &hxcfg);
 
     //2. Power up the hx711 and set gain on chip
-    hx711_power_up(&hx, hx711_gain_128);
+    hx711_power_up(&hx, hx711_gain_32);
 
     //3. This step is optional. Only do this if you want to
     //change the gain AND save it to the HX711 chip
@@ -75,25 +88,30 @@ int main(void) {
     printf("blocking value: %li\n", hx711_get_value(&hx));
 
     // or use a timeout
-    int32_t val;
-    const uint timeout = 250000; //microseconds
-    if(hx711_get_value_timeout(&hx, &val, timeout)) {
-        // value was obtained within the timeout period,
-        // in this case within 250 milliseconds
-        // cppcheck-suppress invalidPrintfArgType_sint
-        printf("timeout value: %li\n", val);
-    }
-    else {
-        printf("value was not obtained within the timeout period\n");
-    }
+    uint32_t start = timer_hw->timelr;
+    while (timer_hw->timelr - start < DELAY_US) {
 
-    // or see if there's a value, but don't block if there isn't one ready
-    if(hx711_get_value_noblock(&hx, &val)) {
-        // cppcheck-suppress invalidPrintfArgType_sint
-        printf("noblock value: %li\n", val);
-    }
-    else {
-        printf("value was not present\n");
+//        printf("blocking value: %li\n", hx711_get_value(&hx));
+
+        int32_t val;
+        const uint timeout = 250000; //microseconds
+        if (hx711_get_value_timeout(&hx, &val, timeout)) {
+            // value was obtained within the timeout period,
+            // in this case within 250 milliseconds
+            // cppcheck-suppress invalidPrintfArgType_sint
+            printf("timeout value: %li\n", val);
+        } else {
+            printf("value was not obtained within the timeout period\n");
+        }
+
+
+        // or see if there's a value, but don't block if there isn't one ready
+        if (hx711_get_value_noblock(&hx, &val)) {
+            // cppcheck-suppress invalidPrintfArgType_sint
+            printf("noblock value: %li\n", val);
+        } else {
+            printf("value was not present\n");
+        }
     }
 
     //6. Stop communication with HX711
@@ -102,12 +120,12 @@ int main(void) {
     printf("Closed communication with single HX711 chip\n");
 #endif
 
-#if 1
+#if 0
     hx711_multi_config_t hxmcfg;
     hx711_multi_get_default_config(&hxmcfg);
     hxmcfg.clock_pin = 14;
-    hxmcfg.data_pin_base = 15;
-    hxmcfg.chips_len = 1;
+    hxmcfg.data_pin_base = 2;
+    hxmcfg.chips_len = 4;
     hxmcfg.pio_irq_index = 1;
     hxmcfg.dma_irq_index = 1;
 
@@ -117,7 +135,7 @@ int main(void) {
     hx711_multi_init(&hxm, &hxmcfg);
 
     // 2. Power up the HX711 chips and set gain on each chip
-    hx711_multi_power_up(&hxm, hx711_gain_128);
+    hx711_multi_power_up(&hxm, hx711_gain_32);
 
     //3. This step is optional. Only do this if you want to
     //change the gain AND save it to each HX711 chip
@@ -145,7 +163,25 @@ int main(void) {
         printf("Failed to obtain values within timeout\n");
     }
 
+    //busy_wait_ms(3000);
+
     hx711_multi_async_start(&hxm);
+
+
+    uint32_t start = timer_hw->timelr;
+    while (timer_hw->timelr - start < DELAY_US) {
+        //printf("hi");
+        while(!hx711_multi_async_done(&hxm)) {
+            // do other stuff...
+            tight_loop_contents();
+        }
+        hx711_multi_async_get_values(&hxm, arr);
+        PRINT_ARR_MathLab(arr, hxmcfg.chips_len);
+        hx711_multi_async_start(&hxm);
+    }
+
+
+
 
     while(!hx711_multi_async_done(&hxm)) {
         // do other stuff...
